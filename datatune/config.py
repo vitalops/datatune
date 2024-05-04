f66d38ed4ffed92fccc2ac6a6cd7f4e1d51e4e61
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2024-05-04 17:05:57 +0530

    Update: add DATATUNE_STORAGE_API_BASE_URL to config

--- datatune/config.py
+++ datatune/config.py
@@ -3,3 +3,6 @@ import os
 # Load environment variables or set default values
 DATATUNE_API_BASE_URL = os.getenv('DATATUNE_API_BASE_URL',
                                   'https://api.datatune.com/v1')
+
+DATATUNE_STORAGE_API_BASE_URL = os.getenv('DATATUNE_STORAGE_API_BASE_URL',
+                                  'https://api.datatune.com/v1/storage')
