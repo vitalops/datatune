5cc123ebd8ae0dbf588d61383cca4d2aa2235243
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2024-05-20 23:37:58 +0530

    update: replace StreamingDataset with custom implementation

--- datatune/config.py
+++ datatune/config.py
@@ -6,3 +6,7 @@ DATATUNE_API_BASE_URL = os.getenv('DATATUNE_API_BASE_URL',
 
 DATATUNE_STORAGE_API_BASE_URL = os.getenv('DATATUNE_STORAGE_API_BASE_URL',
                                   'https://api.datatune.com/v1/storage')
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+S3_ENDPOINT_URL = os.environ['S3_ENDPOINT_URL']
