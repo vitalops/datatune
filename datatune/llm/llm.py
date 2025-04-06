8ca75564ca596c4b89fc7cec991ee2891a731b48
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2025-04-06 18:16:06 +0530

    update: use gemma and tweak prompts

--- datatune/llm/llm.py
+++ datatune/llm/llm.py
@@ -20,7 +20,7 @@ class LLM:
 
 
 class Ollama(LLM):
-    def __init__(self, model_name="llama3.2", api_base="http://localhost:11434", **kwargs) -> None:
+    def __init__(self, model_name="gemma3:4b", api_base="http://localhost:11434", **kwargs) -> None:
         super().__init__(model_name=f"ollama_chat/{model_name}", api_base=api_base, **kwargs)
         self.api_base = api_base
         self._model_name = model_name
