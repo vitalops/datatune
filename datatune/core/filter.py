429eee8c535e4fbb70da0815536af7737f786ba6
Fariz Rahman <farizrahman4u@gmail.com>
2025-04-13 19:49:01 +0530

    dask integration test

--- datatune/core/filter.py
+++ datatune/core/filter.py
@@ -1,4 +1,54 @@
 from typing import Dict, List, Optional, Callable
+from functools import partial
+import numpy as np
+import pandas as pd
+import os
+
+
+_SERIALIZED_INPUT = "_DATATUNE_SERIALIZED_INPUT_"
+_FILTER_PROMPT = "_DATATUNE_FILTER_PROMPT_"
+__LLM_OUTPUT__ = "_DATATUNE_LLM_OUTPUT_"
+
+
+def input_as_string(df: pd.DataFrame) -> pd.DataFrame:
+    df[_SERIALIZED_INPUT] = [str(row.to_dict()) for _, row in df.iterrows()]
+    return df
+
+
+def filter_prompt(prompt: str, df: pd.DataFrame) -> pd.DataFrame:
+    prefix = f"SAY TRUE OR FALSE TO THE FOLLOWING PROMPT:{os.linesep}{prompt}{os.linesep}INPUT: "
+    suffix = (
+        f"{os.linesep}INSTRUCTIONS: OUTPUT JUST TRUE OR FALSE WITHOUT ANY OTHER TEXT" ""
+    )
+    df[_FILTER_PROMPT] = prefix + df[_SERIALIZED_INPUT] + suffix
+    return df
+
+
+def llm_inference(llm: Callable, df: pd.DataFrame) -> pd.DataFrame:
+    df[__LLM_OUTPUT__] = llm(df[_FILTER_PROMPT])
+    return df
+
+
+def parse_filter_output(output: str, err: bool = False) -> Optional[bool]:
+    output = output.strip().upper()
+    if output == "TRUE":
+        return True
+    elif output == "FALSE":
+        return False
+    elif err:
+        raise ValueError(
+            f"Invalid response from LLM: {output}. Expected 'TRUE' or 'FALSE'."
+        )
+    else:
+        return None
+
+
+def parse_filter_output_as_int(result_column: str, df: pd.DataFrame) -> pd.DataFrame:
+    df[__LLM_OUTPUT__] = df[__LLM_OUTPUT__].str.strip().str.upper()
+    df[result_column] = -1
+    df.loc[df[__LLM_OUTPUT__] == "TRUE", result_column] = 1
+    df.loc[df[__LLM_OUTPUT__] == "FALSE", result_column] = 0
+    return df
 
 
 class Filter:
@@ -6,27 +56,12 @@ class Filter:
         self.prompt = prompt
         self.input_fields = input_fields
 
-    def get_full_prompt(self, input: Dict) -> str:
-        return f"""SAY TRUE OR FALSE TO THE FOLLOWING PROMPT:
-{self.prompt}
-INPUT: {input}
-INSTRUCTIONS: OUTPUT JUST TRUE OR FALSE WITHOUT ANY OTHER TEXT"""
-
-    def process_output(self, output: str) -> bool:
-        output = output.strip().upper()
-        if output == "TRUE":
-            return True
-        elif output == "FALSE":
-            return False
-        else:
-            raise ValueError(
-                f"Invalid response from LLM: {output}. Expected 'TRUE' or 'FALSE'."
-            )
-
-    def execute(self, llm: Callable, input: Dict) -> bool:
-        if self.input_fields:
-            input = {field: input[field] for field in self.input_fields}
-        full_prompt = self.get_full_prompt(input)
-        print(f"Full Prompt: {full_prompt}")
-        response = llm(full_prompt)
-        return self.process_output(response)
+    def __call__(self, llm: Callable, df: Dict, result_column: str = "filter_result"):
+        df = df.map_partitions(input_as_string)
+        df = df.map_partitions(
+            partial(filter_prompt, self.prompt),
+        )
+        llm_outputs = df.map_partitions(partial(llm_inference, llm))
+        return llm_outputs.map_partitions(
+            partial(parse_filter_output_as_int, result_column),
+        )
--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,18 +1,15 @@
 from datatune.core.filter import Filter
 from datatune.llm.llm import Ollama
+import pandas as pd
+import dask.dataframe as dd
 
 
-def test_filter_ollama_true():
+def test_filter():
+    df = dd.read_csv("tests/test_data/test_filter.csv")
+    prompt = "check the statement is factually correct"
+    fltr = Filter(prompt=prompt)
     llm = Ollama()
-    prompt = "a > b?"
-    inputs = {"a": 5, "b": 2}
-    result = Filter(prompt=prompt, input_fields=["a", "b"]).execute(llm, inputs)
-    assert result is True, f"Expected True, but got {result}"
-
-
-def test_filter_ollama_false():
-    llm = Ollama()
-    prompt = "a > b?"
-    inputs = {"a": 1, "b": 2}
-    result = Filter(prompt=prompt, input_fields=["a", "b"]).execute(llm, inputs)
-    assert result is False, f"Expected False, but got {result}"
+    filtered = fltr(llm, df, result_column="filter_result")
+    filtered = filtered.head(10)
+    print(filtered)
+    filtered.to_csv("tests/test_data/test_filter_result.csv", index=False)
