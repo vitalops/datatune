189e88cd21f79a344a7a8b61ed3153874b9dbf23
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2025-04-16 01:50:33 +0530

    uppdate: black

--- datatune/core/map.py
+++ datatune/core/map.py
@@ -19,23 +19,23 @@ def input_as_string(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def map_prompt(prompt: str, df: pd.DataFrame) -> pd.DataFrame:
-    prefix = f'''
+    prefix = f"""
     Map and transform the input according to the following prompt.
     :{os.linesep}{prompt}{os.linesep}
-    INPUT: '''
-    suffix = f'''{os.linesep}INSTRUCTIONS:
+    INPUT: """
+    suffix = f"""{os.linesep}INSTRUCTIONS:
     Map and transform the above input according to the above prompt.
     Replace or Create new fields or values as per the prompt.
     Your response MUST be a valid Python dictionary in the format: {{key1: value1, key2: value2, ...}}
     Format your entire response as a valid Python dictionary ONLY with no other text.
-    '''
+    """
     df[_MAP_PROMPT] = prefix + df[_SERIALIZED_INPUT] + suffix
     return df
 
 
 def llm_inference(llm: Callable, df: pd.DataFrame) -> pd.DataFrame:
     prompts = df[_MAP_PROMPT].tolist()
-    
+
     if len(prompts) > 1:
         responses = llm(prompts)
     elif len(prompts) == 1:
@@ -48,10 +48,9 @@ def llm_inference(llm: Callable, df: pd.DataFrame) -> pd.DataFrame:
 
 
 def parse_llm_output(output_str):
-
     if isinstance(output_str, dict):
         return output_str
-            
+
     try:
         return ast.literal_eval(output_str)
     except (SyntaxError, ValueError):
@@ -70,15 +69,15 @@ def update_df_with_llm_output(df: pd.DataFrame, expected_fields=None) -> pd.Data
         expected_fields = list(llm_output.keys())
 
         if not expected_fields:
-            expected_fields = ['result']
-    
+            expected_fields = ["result"]
+
     for field in expected_fields or []:
         if field not in result_df.columns:
             result_df[field] = None
-    
+
     for idx, row in df.iterrows():
         llm_output = parse_llm_output(row[__LLM_OUTPUT__])
-        
+
         for key, value in llm_output.items():
             if key not in result_df.columns:
                 if expected_fields is not None and key not in expected_fields:
@@ -86,22 +85,29 @@ def update_df_with_llm_output(df: pd.DataFrame, expected_fields=None) -> pd.Data
                 result_df[key] = None
             result_df.at[idx, key] = value
 
-    result_df = result_df.drop(columns=[
-        col for col in [_SERIALIZED_INPUT, _MAP_PROMPT, __LLM_OUTPUT__]
-        if col in result_df.columns
-    ])
+    result_df = result_df.drop(
+        columns=[
+            col
+            for col in [_SERIALIZED_INPUT, _MAP_PROMPT, __LLM_OUTPUT__]
+            if col in result_df.columns
+        ]
+    )
 
     return result_df
 
 
 class Map:
-    def __init__(self, prompt: str, input_fields: Optional[List] = None, output_fields: Optional[List] = None):
+    def __init__(
+        self,
+        prompt: str,
+        input_fields: Optional[List] = None,
+        output_fields: Optional[List] = None,
+    ):
         self.prompt = prompt
         self.input_fields = input_fields
         self.output_fields = output_fields
 
     def __call__(self, llm: Callable, df: Dict):
-
         df = df.map_partitions(input_as_string)
         df = df.map_partitions(
             partial(map_prompt, self.prompt),
@@ -110,17 +116,23 @@ class Map:
         df_with_llm = df.map_partitions(partial(llm_inference, llm))
 
         if self.output_fields is None:
-            expected_fields = ['result']
+            expected_fields = ["result"]
         else:
             expected_fields = self.output_fields
 
         input_cols = list(df._meta.columns)
-        output_cols = [col for col in input_cols if col not in [_SERIALIZED_INPUT, _MAP_PROMPT, __LLM_OUTPUT__]]
+        output_cols = [
+            col
+            for col in input_cols
+            if col not in [_SERIALIZED_INPUT, _MAP_PROMPT, __LLM_OUTPUT__]
+        ]
         output_cols.extend(expected_fields)
         meta = pd.DataFrame(columns=output_cols)
 
-        update_func = partial(update_df_with_llm_output, expected_fields=expected_fields)
-        
+        update_func = partial(
+            update_df_with_llm_output, expected_fields=expected_fields
+        )
+
         result_df = df_with_llm.map_partitions(update_func, meta=meta)
-        
+
         return result_df
--- tests/test_map.py
+++ tests/test_map.py
@@ -3,6 +3,7 @@ from datatune.llm.llm import LLM
 import pandas as pd
 import dask.dataframe as dd
 import os
+
 api_key = os.getenv("OPENAI_API_KEY")
 api_base = os.getenv("AZURE_API_BASE")
 api_version = os.getenv("AZURE_API_VERSION")
@@ -12,23 +13,28 @@ def test_map_replace():
     df = dd.read_csv("tests/test_data/test_map.csv")
     prompt = "Replace all personally identifiable terms with XX"
     map = Map(prompt=prompt)
-    llm = LLM(model_name='azure/gpt-35-turbo',
-           api_base=api_base,
-           api_version=api_version,
-           api_key=api_key)
+    llm = LLM(
+        model_name="azure/gpt-35-turbo",
+        api_base=api_base,
+        api_version=api_version,
+        api_key=api_key,
+    )
     mapped = map(llm, df)
     mapped = mapped.head(10)
     print(mapped)
     mapped.to_csv("tests/test_data/test_map_replace_results.csv", index=False)
 
+
 def test_map_create():
     df = dd.read_csv("tests/test_data/test_map.csv")
     prompt = "Calculate the length of each names"
     map = Map(prompt=prompt, output_fields=["first_name_length", "last_name_length"])
-    llm = LLM(model_name='azure/gpt-35-turbo',
-           api_base=api_base,
-           api_version=api_version,
-           api_key=api_key)
+    llm = LLM(
+        model_name="azure/gpt-35-turbo",
+        api_base=api_base,
+        api_version=api_version,
+        api_key=api_key,
+    )
     mapped = map(llm, df)
     mapped = mapped.head(10)
     print(mapped)
