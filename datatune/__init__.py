84ecd9622bf99f1dd26e21265f55c7375bc27e10
Fayaz Rahman <fayazrahman4u@gmail.com>
2025-05-27 21:28:11 +0530

    top level api exposure (#33)

--- datatune/__init__.py
+++ datatune/__init__.py
@@ -1,1 +1,9 @@
-__all__ = []
+from datatune.core.map import Map
+from datatune.core.filter import Filter
+from datatune.core.op import finalize
+
+__all__ = [
+    "Map",
+    "Filter",
+    "finalize"
+]
