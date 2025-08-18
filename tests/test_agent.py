6d2a7f2aa3c858c176cf41f794d32eafa810bb39
fadil4u <fadilrahman4u@gmail.com>
2025-08-19 01:07:56 +0530

    updates

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -12,8 +12,9 @@ def test_agent_dask():
     agent = dt.Agent(llm)
     prompt = "Add a new column called ProfitMargin = (Total Profit / Total Revenue) * 100."
     df = agent.do(prompt,df)
-    result = dt.finalize(df)
     df["ProfitMargin"] = df["ProfitMargin"].astype("float64")
+    result = dt.finalize(df)
+    result.compute().to_csv("agent_dask_results.csv", index=False)
     assert result.columns[-1] == "ProfitMargin"
     assert result["ProfitMargin"].dtype == "float64"
 
@@ -25,13 +26,14 @@ def test_agent_datatune():
     agent = dt.Agent(llm)
     prompt = "Create a new column called Category and Sub-Category based on the Industry column and only keep organizations that are in Africa."
     df = agent.do(prompt,df)
+    df["Category"] = df["Category"].astype("string")
+    df["Sub-Category"] = df["Sub-Category"].astype("string")
     result = dt.finalize(df)
-    #df["Category"] = df["Category"].astype("string")
-    #df["Sub-Category"] = df["Sub-Category"].astype("string")
+    result.compute().to_csv("agent_datatune_results.csv", index=False)
     assert result.columns[-1] == "Sub-Category"
     assert result.columns[-2] == "Category"
-    #assert result["Category"].dtype == "string"
-    #assert result["Sub-Category"].dtype == "string"
+    assert result["Category"].dtype == "string"
+    assert result["Sub-Category"].dtype == "string"
 
 
 def test_agent_combined():
@@ -39,10 +41,11 @@ def test_agent_combined():
     df = dd.read_csv(csv_path)
     llm = OpenAI(model_name="gpt-4o-mini-2024-07-18", tpm=1000000, rpm=5000)
     agent = dt.Agent(llm)
-    prompt = "Create new column called Year for birth year and keep only people who are in STEM related jobs."
+    prompt = "Extract year from date of birth column into a new column called Year and keep only people who are in STEM related jobs."
     df = agent.do(prompt,df)
-    result = dt.finalize(df)
     df["Year"] = df["Year"].astype("int64")
+    result = dt.finalize(df)
+    result.compute().to_csv("agent_combined_results.csv", index=False)
     assert result.columns[-1] == "Year"
-    #assert result["Year"].dtype == "int64"
+    assert result["Year"].dtype == "int64"
     assert len(result) <= len(df)
